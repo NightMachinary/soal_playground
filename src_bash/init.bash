bedfb71f52610e988a18e409080f6aeab48339ce
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-01-18 22:26:29 +0330

    modified: init.bash

--- src_bash/init.bash
+++ src_bash/init.bash
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-
+# @UbuntuOnly
+##
 apt install -y unzip aria2 ncdu htop
+##
+wget https://github.com/sharkdp/hyperfine/releases/download/v1.12.0/hyperfine_1.12.0_amd64.deb
+sudo dpkg -i hyperfine_1.12.0_amd64.deb
+##
