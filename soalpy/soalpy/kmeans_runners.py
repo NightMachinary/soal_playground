75a3706b898970daaef1a65697fbbf95ada3112c
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-01-18 23:03:29 +0330

    modified: kmeans_runners.py; modified: init.bash

--- soalpy/soalpy/kmeans_runners.py
+++ soalpy/soalpy/kmeans_runners.py
@@ -1,6 +1,12 @@
 from .utils import *
 
-def kmeans_sklearn_n10_iter10(input_data, target_data=None):
-  clf = KMeans(n_clusters=10, max_iter=10)
+def kmeans_sklearn(input_data, target_data=None, **kwargs):
+  clf = KMeans(**kwargs)
   clf.fit(input_data)
   return {'loss': clf.inertia_}
+
+def kmeans_sklearn_n10_iter10(input_data, target_data=None):
+  return kmeans_sklearn(input_data, target_data, n_clusters=10, max_iter=10)
+
+def kmeans_sklearn_n10_iter10e4(input_data, target_data=None):
+  return kmeans_sklearn(input_data, target_data, n_clusters=10, max_iter=10**4)
