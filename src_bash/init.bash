b4d46607785886bcf29748847ad12177cc2f7242
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-01-19 20:36:12 +0330

    fixed mistake in previous commit

--- src_bash/init.bash
+++ src_bash/init.bash
@@ -8,5 +8,5 @@ sudo dpkg -i hyperfine_1.12.0_amd64.deb
 ##
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-*.sh -b -p "$HOME/miniconda3"
-echo 'export PATH="$HOME/miniconda3:$PATH"' >> ~/.zshenv
+echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.zshenv
 ##
