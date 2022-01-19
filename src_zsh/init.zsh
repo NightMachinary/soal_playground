66519173537fd1e3e9bd31632ea9b85db380dcac
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-01-19 21:12:44 +0330

    modified: init.zsh

--- src_zsh/init.zsh
+++ src_zsh/init.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 conda install -y -c rapidsai -c nvidia -c conda-forge \
-    python=3.8 rapids=21.12 cudatoolkit=11.5 dask-sql
+    python=3.8 rapids=21.12 cudatoolkit=11.0 dask-sql
