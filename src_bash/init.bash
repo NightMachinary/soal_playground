b6a3ed31c81e4d0e3fc27d96ac148c63f92e7030
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-01-24 18:44:24 +0330

    modified: init.bash; new t1.py

--- src_bash/init.bash
+++ src_bash/init.bash
@@ -14,3 +14,19 @@ bash ./Miniconda3-latest-*.sh -f -b -p "/usr/local"
 # bash ./Miniconda3-latest-*.sh -b -p "$HOME/miniconda3"
 # echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.zshenv
 ##
+#: This silences the large allocation warnings in Python.
+echo 'export TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD=21329330176' >> ~/.zshenv
+##
+nvtop-install () {
+    sudo apt-get install -y cmake libncurses5-dev libncursesw5-dev git || return $?
+    local d=~/code/misc
+    mkdir -p "$d" || return $?
+    cd "$d" || return $?
+    git clone https://github.com/Syllo/nvtop.git || return $?
+    mkdir -p nvtop/build && cd nvtop/build || return $?
+    cmake .. || return $?
+    make || return $?
+    sudo make install || return $?
+}
+nvtop-install
+##
---
+++ src_py/t1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import numpy
+np = numpy
+import sys
+
+import dask_ml
+import dask_ml.cluster
+from dask_ml.datasets import make_blobs as dask_make_blobs
+##
+X, y = dask_make_blobs(
+    n_samples=10**3,
+    n_features=2,
+    # n_samples=10**6,
+    # n_features=10**4,
+    centers=10,
+    chunks=(10**4, 10**4),
+    # chunks=(10**4, 10**4),
+    )
+X = X.astype(np.float32)
+y = y.astype(np.float32)
+##
+import cupy
+
+X = X.map_blocks(cupy.asarray)
+##
+from dask_cuda import LocalCUDACluster
+from dask.distributed import Client
+import dask
+
+cluster = LocalCUDACluster()
+client = Client(cluster)
+##
+from cuml.dask.cluster import KMeans as cuKMeans
+
+km_cu = cuKMeans(n_clusters=10)
+km_cu.fit(X)
