8c5897c13b0ac2855c26dd559f42a4ea3f39791b
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-05-24 19:53:19 +0430

    new all_khordad_3.png; modified: gmm_runners.py

--- soalpy/soalpy/gmm_runners.py
+++ soalpy/soalpy/gmm_runners.py
@@ -1,6 +1,6 @@
 from .utils import *
 from .runners import *
 ##
-def gmm_sklearn_best(dataset, **kwargs):
+def gmm_sklearn_cov_full(dataset, **kwargs):
     return run(dataset, mode="GMM", **kwargs,)
 ##
